5b4e152633522992f14e87e004c8b1301f449221
Edson Joel López Quispe <elopezqu@unsa.edu.pe>
2022-05-09 09:51:38 -0500

    Se agrega los dias de la semana si cumple que se ingreso el dia actual

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,5 +1,23 @@
 function textoDia(a){
 	const fechaA = new Date();
 	let num = fechaA.getDay();
-	return num;
+	let dia = "No coincide con la fecha actual";
+	if(a==num){
+		switch(a){
+			case 0 : dia = "Domingo";
+				break;
+			case 1 : dia = "Lunes";
+				break;
+			case 2 : dia = "Martes";
+				break;
+			case 3 : dia = "Miercoles";
+				break;
+			case 4 : dia = "Jueves";
+				break;
+			case 5 : dia = "Viernes";
+				break;
+			case 6 : dia = "Sábado";
+		}
+	}
+	return dia;
 }
