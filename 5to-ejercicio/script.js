7b9d188da461af6ed7b4527c34cc46b7762a0970
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-05-09 21:28:17 -0500

    script.js: implementado generacion de valores dinamicos en la tabla, mostrando al final la suma de ellos

--- 5to-ejercicio/script.js
+++ 5to-ejercicio/script.js
@@ -5,18 +5,20 @@ function generarTabla(){
 
 	contenedorTabla.innnerHTML = "";
 	var tabla = "<table>";
-	
+	var valor = 0;
+	var suma = 0;
+
 	for(var i = 1; i <= numFilas; i++){
 		tabla += "<tr>";
 		for(var j = 1; j <= numColumnas; j++){
 			valor = Math.floor(Math.random() * 100);
-			tabla += "<td>"+"V"+"</br></td>";
-			
+			tabla += "<td>"+valor+"</br></td>";
+			suma+=valor;
 		}
 		tabla += "</tr>";
 	}
-	tabla += "</table></br>";
-	
+	tabla += "</table></br>Resultado es: ";
+	tabla+=suma;
 	contenedorTabla.innerHTML = tabla;
 	
 }
